54e5d88998181ea2116f608fe6331af30dca08af
Simon Conseil <contact@saimon.org>
2018-07-11 18:58:25 +0200

    Add dev version

---
+++ musered/version.py
@@ -0,0 +1,26 @@
+import os
+
+__version__ = '0.1.dev'
+__description__ = 'Muse data reduction, quick and easy'
+
+CURDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def _update_git_version():
+    from contextlib import suppress
+    from subprocess import check_output
+    command_number = 'git -C {} rev-list --count HEAD'.format(CURDIR).split()
+    with suppress(Exception):
+        return check_output(command_number).decode('ascii').strip()
+
+
+try:
+    if '.dev' in __version__:
+        commit_number = _update_git_version()
+        if commit_number:
+            __version__ += commit_number
+        else:
+            from ._githash import __dev_value__
+            __version__ += __dev_value__
+except Exception:
+    pass
--- setup.py
+++ setup.py
@@ -1,14 +1,48 @@
+import os
+import subprocess
+import sys
 from setuptools import setup, find_packages
 
-# Read version from version.py
-# __version__ = None
-# with open('mydrs/version.py') as f:
-#     exec(f.read())
+if sys.version_info < (3, 6):
+    raise Exception('python 3.6 or newer is required')
+
+# Read version.py
+__version__ = None
+__description__ = None
+with open('musered/version.py') as f:
+    exec(f.read())
+
+# If the version is not stable, we can add a git hash to the __version__
+if '.dev' in __version__:
+    # Find hash for __githash__ and dev number for __version__ (can't use hash
+    # as per PEP440)
+    command_hash = 'git rev-list --max-count=1 --abbrev-commit HEAD'
+    command_number = 'git rev-list --count HEAD'
+
+    try:
+        commit_hash = subprocess.check_output(command_hash, shell=True)\
+            .decode('ascii').strip()
+        commit_number = subprocess.check_output(command_number, shell=True)\
+            .decode('ascii').strip()
+    except Exception:
+        pass
+    else:
+        # We write the git hash and value so that they gets frozen if installed
+        with open(os.path.join('musered', '_githash.py'), 'w') as f:
+            f.write("__githash__ = \"{}\"\n".format(commit_hash))
+            f.write("__dev_value__ = \"{}\"\n".format(commit_number))
+
+        # We modify __version__ here too for commands such as egg_info
+        # __version__ += commit_number
 
 setup(
     name='musered',
-    version='0.1dev',
+    version=__version__,
+    description=__description__,
+    author='Simon Conseil',
+    author_email='simon.conseil@univ-lyon1.fr',
     packages=find_packages(),
+    include_package_data=True,
     zip_safe=False,
     install_requires=['numpy', 'matplotlib', 'astropy', 'python-cpl', 'tqdm',
                       'mpdaf', 'sqlalchemy', 'dataset', 'PyYAML', 'click'],
