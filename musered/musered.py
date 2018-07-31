1882427b62bb45d8c86336fecc6c7c8c3405219d
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-07-31 16:33:09 +0200

    Move StaticCalib in a module

--- musered/musered.py
+++ musered/musered.py
@@ -15,62 +15,9 @@ from mpdaf.log import setup_logging
 from sqlalchemy import sql, func
 
 from .settings import RAW_FITS_KEYWORDS, STATIC_FRAMES
+from .static_calib import StaticCalib
 from .utils import (load_yaml_config, load_db, get_exp_name,
                     parse_date, parse_datetime, NOON, ONEDAY, ProgressBar)
-
-
-class StaticCalib:
-    """Manage static calibrations.
-
-    It must be instantiated with a directory containing the default static
-    calibration files, and a settings dict that can be used to define time
-    periods where a given calibration file is valid.
-    """
-
-    def __init__(self, path, conf):
-        self.path = path
-        self.conf = conf
-
-    @lazyproperty
-    def files(self):
-        return os.listdir(self.path)
-
-    @lazyproperty
-    def catg_list(self):
-        cat = defaultdict(list)
-        for f in self.files:
-            key = fits.getval(os.path.join(self.path, f),
-                              'ESO PRO CATG', ext=0)
-            cat[key].append(f)
-        return cat
-
-    def get(self, key, date=None):
-        file = None
-        if key in self.conf:
-            # if key is defined in the conf file, try to find a static calib
-            # file that matched the date requirement
-            for item, val in self.conf[key].items():
-                if date is None:
-                    file = item
-                    break
-                date = parse_date(date)
-                start_date = val.get('start_date', datetime.date.min)
-                end_date = val.get('end_date', datetime.date.max)
-                if start_date < date < end_date:
-                    file = item
-                    break
-
-        if file is None:
-            # found nothing, use default from the static calib directory
-            if len(self.catg_list[key]) > 1:
-                logger = logging.getLogger(__name__)
-                logger.warning('multiple options for %s, using the first '
-                               'one: %r', key, self.catg_list[key])
-            file = self.catg_list[key][0]
-
-        if file not in self.files:
-            raise ValueError(f'could not find {file}')
-        return os.path.join(self.path, file)
 
 
 class MuseRed:
