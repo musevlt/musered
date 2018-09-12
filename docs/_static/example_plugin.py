391c5bbd58f0aa90e413745d2571e4a5de16a213
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-09-12 17:28:54 +0200

    Add examples for plugins

---
+++ docs/_static/example_plugin.py
@@ -0,0 +1,14 @@
+import click
+import logging
+
+
+@click.argument('name')
+@click.pass_obj
+def my_command(mr, name):
+    """Example plugin, say hello."""
+    logger = logging.getLogger('musered')
+    logger.info('Hello %s!', name)
+
+    # mr is the MuseRed object, it can be used to access the database
+    logger.info('raw: %d rows', mr.raw.count())
+    logger.info('reduced: %d rows', mr.reduced.count())
