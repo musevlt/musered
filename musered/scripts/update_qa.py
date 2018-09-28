3af3a0d9b2e1cc53f859b49e317d2c362c43e5e5
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-09-28 10:44:11 +0200

    Cleanup a bit update_qa, use normalize_recipe_name

--- musered/scripts/update_qa.py
+++ musered/scripts/update_qa.py
@@ -1,23 +1,25 @@
-
 import click
-import inspect
-import logging
-import os
-import sys
 import itertools
-import pprint
-from astropy.table import Table
+import logging
 import numpy as np
+import pprint
 import psfrec
-import json
+from astropy.table import Table
+
+from ..utils import normalize_recipe_name
 
 logger = logging.getLogger(__name__)
 
+
 @click.argument('date', nargs=-1)
-@click.option('--sky', is_flag=True, help='update qa_reduced table with sky flux in B,V,R')
-@click.option('--sparta', is_flag=True, help='update qa_raw table with average sparta seeing and gl data')
-@click.option('--imphot', is_flag=True, help='update qa_reduced table with imphot values')
-@click.option('--psfrec', is_flag=True, help='run PSF reconstruction and update qa_raw table')
+@click.option('--sky', is_flag=True,
+              help='update qa_reduced table with sky flux in B,V,R')
+@click.option('--sparta', is_flag=True,
+              help='update qa_raw table with average sparta seeing and gl data')
+@click.option('--imphot', is_flag=True,
+              help='update qa_reduced table with imphot values')
+@click.option('--psfrec', is_flag=True,
+              help='run PSF reconstruction and update qa_raw table')
 @click.option('--recipe', help='recipe name')
 @click.option('--force', is_flag=True, help="force update of database")
 @click.option('--dry-run', is_flag=True, help="don't update the database")
@@ -27,7 +29,6 @@ def update_qa(mr, date, sky, sparta, imphot, psfrec, recipe, force, dry_run):
 
     if len(date) == 0:
         dates = list(itertools.chain.from_iterable(mr.exposures.values()))
-        exists = [row['name'] for row in mr.qa_raw.find() if row['SP_See'] is not None] 
     else:
         dates = mr._prepare_dates(date, 'OBJECT', 'name')
 
@@ -46,10 +47,10 @@ def update_qa(mr, date, sky, sparta, imphot, psfrec, recipe, force, dry_run):
 def qa_imphot(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
     if recipe_name is None:
         recipe_name = 'muse_exp_align'
-    elif not recipe_name.startswith('muse_'):
-        recipe_name = 'muse_' + recipe_name
 
-    rows = mr.reduced.find(recipe_name=recipe_name, DPR_TYPE='IMPHOT', name=dates)
+    recipe_name = normalize_recipe_name(recipe_name)
+    rows = mr.reduced.find(recipe_name=recipe_name, DPR_TYPE='IMPHOT',
+                           name=dates)
     if skip:
         exists = _find_existing_exp(mr.qa_reduced, 'IM_vers')
     qarows = []
@@ -60,7 +61,7 @@ def qa_imphot(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
         imphot = _imphot(tabname)
         imphot['IM_vers'] = row['recipe_version']
         logger.debug('Name %s Imphot %s', row['name'], imphot)
-        qarows.append({'name':row['name'], **imphot})
+        qarows.append({'name': row['name'], **imphot})
     if dry_run:
         pprint.pprint(qarows)
     else:
@@ -68,13 +69,14 @@ def qa_imphot(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
             for row in qarows:
                 tx[mr.tables['qa_reduced']].upsert(row, ['name'])
 
-def qa_sky(mr, dates=None, skip=True, dry_run=False):
 
+def qa_sky(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
     if recipe_name is None:
         recipe_name = 'muse_scipost'
-    elif not recipe_name.startswith('muse_'):
-        recipe_name = 'muse_' + recipe_name
-    rows = mr.reduced.find(recipe_name=recipe_name, DPR_TYPE='SKY_SPECTRUM', name=dates)
+
+    recipe_name = normalize_recipe_name(recipe_name)
+    rows = mr.reduced.find(recipe_name=recipe_name, DPR_TYPE='SKY_SPECTRUM',
+                           name=dates)
     if skip:
         exists = _find_existing_exp(mr.qa_reduced, 'skyB')
     qarows = []
@@ -82,10 +84,10 @@ def qa_sky(mr, dates=None, skip=True, dry_run=False):
         if skip and row['name'] in exists:
             continue
         skyname = row['path'] + '/SKY_SPECTRUM_0001.fits'
-        skytab =  Table.read(skyname)
+        skytab = Table.read(skyname)
         skyflux = _sky(skytab)
         logger.debug('Name %s Sky %s', row['name'], skyflux)
-        qarows.append({'name':row['name'], **skyflux})
+        qarows.append({'name': row['name'], **skyflux})
     if dry_run:
         pprint.pprint(qarows)
     else:
@@ -102,11 +104,11 @@ def qa_sparta(mr, dates=None, skip=True, dry_run=False):
     for row in rows:
         if skip and row['name'] in exists:
             continue
-        rawname = row['path'] 
-        tab =  Table.read(rawname, hdu='SPARTA_ATM_DATA')
+        rawname = row['path']
+        tab = Table.read(rawname, hdu='SPARTA_ATM_DATA')
         sparta_dict = _sparta(tab)
         logger.debug('Name %s SPARTA %s', row['name'], sparta_dict)
-        qarows.append({'name':row['name'], **sparta_dict})
+        qarows.append({'name': row['name'], **sparta_dict})
     if dry_run:
         pprint.pprint(qarows)
     else:
@@ -114,48 +116,61 @@ def qa_sparta(mr, dates=None, skip=True, dry_run=False):
             for row in qarows:
                 tx['qa_raw'].upsert(row, ['name'])
 
+
 def qa_psfrec(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
     rows = mr.raw.find(name=dates)
     if skip:
         exists = _find_existing_exp(mr.qa_raw, 'PR_vers')
-    qarows = []
     for row in rows:
         if skip and row['name'] in exists:
             continue
-        rawname = row['path'] 
+        rawname = row['path']
         psfrec_dict = _psfrec(rawname)
         logger.debug('Name %s PSFRec %s', row['name'], psfrec_dict)
         if not dry_run:
-            mr.qa_raw.upsert({'name':row['name'], **psfrec_dict}, ['name'])
+            mr.qa_raw.upsert({'name': row['name'], **psfrec_dict}, ['name'])
+
 
 def _find_existing_exp(table, key):
     if key not in table.columns:
         return []
-    exists = [row['name'] for row in table.find() if row[key] is not None] 
+    exists = [row['name'] for row in table.find() if row[key] is not None]
     return exists
 
+
 def _psfrec(filename):
-    lbda,fwhm,beta = psfrec.reconstruct_psf(filename)
-    lbref = [500,700,900]
+    lbda, fwhm, beta = psfrec.reconstruct_psf(filename)
+    lbref = [500, 700, 900]
     fwhmref = np.interp(lbref, lbda, fwhm)
     betaref = np.interp(lbref, lbda, beta)
-    psfdict = {'PR_vers':psfrec.__version__, 'PR_fwhmB':fwhmref[0], 'PR_fwhmV':fwhmref[1], 'PR_fwhmR':fwhmref[2],
-               'PR_betaB':betaref[0], 'PR_betaV':betaref[1], 'PR_betaR':betaref[2]}
+    psfdict = {
+        'PR_vers': psfrec.__version__,
+        'PR_fwhmB': fwhmref[0],
+        'PR_fwhmV': fwhmref[1],
+        'PR_fwhmR': fwhmref[2],
+        'PR_betaB': betaref[0],
+        'PR_betaV': betaref[1],
+        'PR_betaR': betaref[2]
+    }
     return psfdict
 
 
 def _sky(s):
     skyflux = {}
-    for band,l1,l2 in [['skyB',4850,6000],['skyV',6000,8000],['skyR',8000,9300]]:
-        flux = float(s['data'][(s['lambda']>=l1) & (s['lambda']<=l2)].mean())
+    for band, l1, l2 in [['skyB', 4850, 6000],
+                         ['skyV', 6000, 8000],
+                         ['skyR', 8000, 9300]]:
+        flux = float(s['data'][(s['lambda'] >= l1) &
+                               (s['lambda'] <= l2)].mean())
         skyflux[band] = flux
     return skyflux
 
+
 def _sparta(tab):
     res = {}
-    klist = [k for k in range(1,5) if tab[f'LGS{k}_TUR_GND'][0] > 0]
+    klist = [k for k in range(1, 5) if tab[f'LGS{k}_TUR_GND'][0] > 0]
     if len(klist) < 4:
-         logger.warning('Mode %d lasers detected', len(klist))
+        logger.warning('Mode %d lasers detected', len(klist))
     s = [tab[f'LGS{k}_SEEING'].mean() for k in klist]
     res['SP_See'] = float(np.mean(s))
     res['SP_SeeStd'] = float(np.std(s))
@@ -164,12 +179,11 @@ def _sparta(tab):
     res['SP_GlStd'] = float(np.std(g))
     return res
 
+
 def _imphot(tabname):
     tab = Table.read(tabname)
     res = {}
-    row = tab[tab['filter']=='F775W'][0]
-    for key in ['fwhm','beta','bg','scale','dx','dy','rms']:
-        res['IM_'+key] = row[key]
+    row = tab[tab['filter'] == 'F775W'][0]
+    for key in ['fwhm', 'beta', 'bg', 'scale', 'dx', 'dy', 'rms']:
+        res['IM_' + key] = row[key]
     return res
-
-
