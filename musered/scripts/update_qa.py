a9834edf3e677e2a0683c40e04b4bc6e69a0d4c5
Roland bacon <roland.bacon@univ-lyon1.fr>
2018-09-28 08:53:17 +0200

    add --imphot, use _find_existing_exps to serach for exp to skip

--- musered/scripts/update_qa.py
+++ musered/scripts/update_qa.py
@@ -1,3 +1,4 @@
+
 import click
 import inspect
 import logging
@@ -8,22 +9,25 @@ import pprint
 from astropy.table import Table
 import numpy as np
 import psfrec
+import json
 
 logger = logging.getLogger(__name__)
 
 @click.argument('date', nargs=-1)
 @click.option('--sky', is_flag=True, help='update qa_reduced table with sky flux in B,V,R')
 @click.option('--sparta', is_flag=True, help='update qa_raw table with average sparta seeing and gl data')
+@click.option('--imphot', is_flag=True, help='update qa_reduced table with imphot values')
 @click.option('--psfrec', is_flag=True, help='run PSF reconstruction and update qa_raw table')
 @click.option('--recipe', help='recipe name')
 @click.option('--force', is_flag=True, help="force update of database")
 @click.option('--dry-run', is_flag=True, help="don't update the database")
 @click.pass_obj
-def update_qa(mr, date, sky, sparta, psfrec, recipe, force, dry_run):
+def update_qa(mr, date, sky, sparta, imphot, psfrec, recipe, force, dry_run):
     """Update QA databases (qa_raw and qa_reduced)."""
 
     if len(date) == 0:
         dates = list(itertools.chain.from_iterable(mr.exposures.values()))
+        exists = [row['name'] for row in mr.qa_raw.find() if row['SP_See'] is not None] 
     else:
         dates = mr._prepare_dates(date, 'OBJECT', 'name')
 
@@ -35,18 +39,44 @@ def update_qa(mr, date, sky, sparta, psfrec, recipe, force, dry_run):
         qa_sparta(mr, **kwargs)
     if psfrec:
         qa_psfrec(mr, **kwargs)
+    if imphot:
+        qa_imphot(mr, **kwargs)
 
 
-def qa_sky(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
+def qa_imphot(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
+    if recipe_name is None:
+        recipe_name = 'muse_exp_align'
+    elif not recipe_name.startswith('muse_'):
+        recipe_name = 'muse_' + recipe_name
+
+    rows = mr.reduced.find(recipe_name=recipe_name, DPR_TYPE='IMPHOT', name=dates)
+    if skip:
+        exists = _find_existing_exp(mr.qa_reduced, 'IM_vers')
+    qarows = []
+    for row in rows:
+        if skip and row['name'] in exists:
+            continue
+        tabname = f"{row['path']}/IMPHOT.fits"
+        imphot = _imphot(tabname)
+        imphot['IM_vers'] = row['recipe_version']
+        logger.debug('Name %s Imphot %s', row['name'], imphot)
+        qarows.append({'name':row['name'], **imphot})
+    if dry_run:
+        pprint.pprint(qarows)
+    else:
+        with mr.db as tx:
+            for row in qarows:
+                tx[mr.tables['qa_reduced']].upsert(row, ['name'])
+
+def qa_sky(mr, dates=None, skip=True, dry_run=False):
 
     if recipe_name is None:
         recipe_name = 'muse_scipost'
     elif not recipe_name.startswith('muse_'):
         recipe_name = 'muse_' + recipe_name
-
     rows = mr.reduced.find(recipe_name=recipe_name, DPR_TYPE='SKY_SPECTRUM', name=dates)
     if skip:
-        exists = [row['name'] for row in mr.qa_reduced.find() if row['skyB'] is not None] 
+        exists = _find_existing_exp(mr.qa_reduced, 'skyB')
     qarows = []
     for row in rows:
         if skip and row['name'] in exists:
@@ -63,10 +93,11 @@ def qa_sky(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
             for row in qarows:
                 tx[mr.tables['qa_reduced']].upsert(row, ['name'])
 
+
 def qa_sparta(mr, dates=None, skip=True, dry_run=False):
     rows = mr.raw.find(name=dates)
     if skip:
-        exists = [row['name'] for row in mr.qa_raw.find() if row['SP_See'] is not None] 
+        exists = _find_existing_exp(mr.qa_raw, 'SP_see')
     qarows = []
     for row in rows:
         if skip and row['name'] in exists:
@@ -86,7 +117,7 @@ def qa_sparta(mr, dates=None, skip=True, dry_run=False):
 def qa_psfrec(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
     rows = mr.raw.find(name=dates)
     if skip:
-        exists = [row['name'] for row in mr.qa_raw.find() if row['PR_vers'] is not None] 
+        exists = _find_existing_exp(mr.qa_raw, 'PR_vers')
     qarows = []
     for row in rows:
         if skip and row['name'] in exists:
@@ -96,6 +127,12 @@ def qa_psfrec(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
         logger.debug('Name %s PSFRec %s', row['name'], psfrec_dict)
         if not dry_run:
             mr.qa_raw.upsert({'name':row['name'], **psfrec_dict}, ['name'])
+
+def _find_existing_exp(table, key):
+    if key not in table.columns:
+        return []
+    exists = [row['name'] for row in table.find() if row[key] is not None] 
+    return exists
 
 def _psfrec(filename):
     lbda,fwhm,beta = psfrec.reconstruct_psf(filename)
@@ -126,3 +163,13 @@ def _sparta(tab):
     res['SP_Gl'] = float(np.mean(g))
     res['SP_GlStd'] = float(np.std(g))
     return res
+
+def _imphot(tabname):
+    tab = Table.read(tabname)
+    res = {}
+    row = tab[tab['filter']=='F775W'][0]
+    for key in ['fwhm','beta','bg','scale','dx','dy','rms']:
+        res['IM_'+key] = row[key]
+    return res
+
+
