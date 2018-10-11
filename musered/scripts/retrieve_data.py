c9ec7d8ea1f1c45df2db2fd0ac3c90355c5f92ef
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-10-11 16:53:53 +0200

    Add more options to retrieve_data:
    - Retrieve calibrations for a list of nights with --calib and --ins-mode
    - Specify start and end date with --start and --end
    
    Also limit the number of columns printed to stdout to make this more
    readable

--- musered/scripts/retrieve_data.py
+++ musered/scripts/retrieve_data.py
@@ -11,15 +11,38 @@ import sys
 @click.option('--dry-run', is_flag=True, help="don't retrieve files")
 @click.option('--force', is_flag=True, help="request all objects")
 @click.option('--no-update-db', is_flag=True, help="don't update the database")
+@click.option('--calib', help='retrieve only calibrations for a list of '
+              'comma-separated nights (YYYY MM(M) DD)')
+@click.option('--ins-mode', help='instrument mode for calib retrieval')
+@click.option('--start', help='start date, to limit query results')
+@click.option('--end', help='end date, to limit query results')
 @click.pass_obj
 def retrieve_data(mr, dataset, username, help_query, dry_run, force,
-                  no_update_db):
-    """Retrieve files from DATASET from the ESO archive."""
+                  no_update_db, calib, ins_mode, start, end):
+    """Retrieve files and/or calibrations from the ESO archive.
 
+    By default this will retrieve science files and associated calibrations,
+    with the filters defined in the settings files. It is also possible to
+    retrieve all the calibration files for a list of nights (with --calib) and
+    for an instrument mode (--ins-mode).
+
+    Note that the definition of nights is different for MuseRed and for ESO:
+
+    - for MuseRed, the 2017-06-19 night corresponds to the day calibrations of
+      2017-06-20
+    - for ESO, the 2017-06-19 night corresponds to the day calibrations of
+      2017-06-19
+
+    Examples::
+
+        $ musered retrieve-data --start 2017-06-19 IC4406
+
+        $ musered retrieve-data --calib 2017-06-19 --ins-mode WFM-AO-N
+
+    """
     logger = logging.getLogger(__name__)
     if len(dataset) == 0:
-        logger.error('You must provide at least one dataset')
-        sys.exit(1)
+        dataset = list(mr.datasets)
 
     params = mr.conf['retrieve_data']
     if username is not None:
@@ -40,29 +63,47 @@ def retrieve_data(mr, dataset, username, help_query, dry_run, force,
     Eso.cache_location = os.path.join(mr.raw_path, '.cache')
     os.makedirs(Eso.cache_location, exist_ok=True)
 
-    for ds in dataset:
-        if ds not in mr.datasets:
-            logger.error("dataset '%s' not found", ds)
-            sys.exit(1)
+    if calib:
+        dataset = calib.split(',')
 
-        conf = mr.datasets[ds]
-        table = Eso.query_instrument(
-            'muse',
-            cache=conf.get('cache', False),
-            # columns=conf.get('columns', []),  # does not work
-            column_filters=conf['archive_filter']
-        )
-        logger.info('Found %d exposures', len(table))
-        logger.debug('\n'.join(table['DP.ID']))
+    for ds in dataset:
+        if calib:
+            ds = ds.replace('-', ' ')
+            column_filters = {'night': ds, 'dp_cat': 'CALIB'}
+            if ins_mode is not None:
+                column_filters['ins_mode'] = ins_mode
+        else:
+            if ds not in mr.datasets:
+                logger.error("dataset '%s' not found", ds)
+                sys.exit(1)
+            conf = mr.datasets[ds]
+            column_filters = conf['archive_filter']
+
+        if start:
+            column_filters['stime'] = start.replace('-', ' ')
+        if end:
+            column_filters['etime'] = end.replace('-', ' ')
+
+        logger.info('Searching files for %s', ds)
+        logger.info('Filters: %s', column_filters)
+        table = Eso.query_instrument('muse', cache=False,
+                                     column_filters=column_filters)
+        if table is None:
+            logger.warning('Found nothing')
+            continue
+        logger.info('Found %d files', len(table))
+        table.keep_columns(['Object', 'Target Ra Dec', 'ProgId', 'DP.ID',
+                            'EXPTIME [s]', 'DPR CATG', 'DPR TYPE', 'DPR TECH',
+                            'TPL START', 'INS MODE', 'DIMM Seeing-avg'])
         print(table)
         if not dry_run:
-            sig = inspect.signature(Eso.retrieve_data)
-            if 'request_all_objects' in sig.parameters:
-                Eso.retrieve_data(table['DP.ID'], destination=mr.raw_path,
-                                  with_calib='raw', request_all_objects=force)
-            else:
-                Eso.retrieve_data(table['DP.ID'], destination=mr.raw_path,
-                                  with_calib='raw')
+            kw = dict(destination=mr.raw_path)
+            if not calib:
+                kw['with_calib'] = 'raw'
+                sig = inspect.signature(Eso.retrieve_data)
+                if 'request_all_objects' in sig.parameters:
+                    kw['request_all_objects'] = force
+            Eso.retrieve_data(table['DP.ID'], **kw)
 
     if not dry_run and not no_update_db:
         mr.update_db()
