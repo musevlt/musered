cd5204068ded9b959e16a97ae5a68ecc11f296b6
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-12-17 15:49:38 +0100

    Simplify a few things in update_qa

--- musered/scripts/update_qa.py
+++ musered/scripts/update_qa.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 @click.option('--sky', is_flag=True,
               help='update qa_reduced table with sky flux in B,V,R')
 @click.option('--sparta', is_flag=True,
-              help='update qa_raw table with average sparta seeing and gl data')
+              help='update qa_raw table with average sparta seeing and GL')
 @click.option('--imphot', is_flag=True,
               help='update qa_reduced table with imphot values')
 @click.option('--psfrec', is_flag=True,
@@ -49,22 +49,21 @@ def qa_imphot(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
         recipe_name = 'imphot'
 
     rows = list(mr.reduced.find(recipe_name=recipe_name, DPR_TYPE='IMPHOT',
-                           name=dates))
+                                name=dates))
     if skip:
         exists = _find_existing_exp(mr.qa_reduced, 'IM_vers')
         rows = [row for row in rows if row['name'] not in exists]
     logger.info(f'found {len(rows)} exposures in database to process')
     qarows = []
     for row in rows:
-        tabname = f"{row['path']}/IMPHOT.fits"
-        imphot = _imphot(tabname)
+        imphot = _imphot(f"{row['path']}/IMPHOT.fits")
         imphot['IM_vers'] = row['recipe_version']
         logger.debug('Name %s Imphot %s', row['name'], imphot)
         qarows.append({'name': row['name'], **imphot})
     if dry_run:
         pprint.pprint(qarows)
     else:
-        upsert_many(mr.db, mr.tables['qa_reduced'], qarows, ['name'])
+        upsert_many(mr.db, mr.qa_reduced, qarows, ['name'])
 
 
 def qa_sky(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
@@ -72,23 +71,21 @@ def qa_sky(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
         recipe_name = 'muse_scipost'
 
     recipe_name = normalize_recipe_name(recipe_name)
-    rows = list(mr.reduced.find(recipe_name=recipe_name, DPR_TYPE='SKY_SPECTRUM',
-                           name=dates))
+    rows = list(mr.reduced.find(recipe_name=recipe_name,
+                                DPR_TYPE='SKY_SPECTRUM', name=dates))
     if skip:
         exists = _find_existing_exp(mr.qa_reduced, 'skyB')
         rows = [row for row in rows if row['name'] not in exists]
     logger.info(f'found {len(rows)} exposures in database to process')
     qarows = []
     for row in rows:
-        skyname = row['path'] + '/SKY_SPECTRUM_0001.fits'
-        skytab = Table.read(skyname)
-        skyflux = _sky(skytab)
+        skyflux = _sky(f"{row['path']}/SKY_SPECTRUM_0001.fits")
         logger.debug('Name %s Sky %s', row['name'], skyflux)
         qarows.append({'name': row['name'], **skyflux})
     if dry_run:
         pprint.pprint(qarows)
     else:
-        upsert_many(mr.db, mr.tables['qa_reduced'], qarows, ['name'])
+        upsert_many(mr.db, mr.qa_reduced, qarows, ['name'])
 
 
 def qa_sparta(mr, dates=None, skip=True, dry_run=False):
@@ -99,15 +96,13 @@ def qa_sparta(mr, dates=None, skip=True, dry_run=False):
     logger.info(f'found {len(rows)} exposures in database to process')
     qarows = []
     for row in rows:
-        rawname = row['path']
-        tab = Table.read(rawname, hdu='SPARTA_ATM_DATA')
-        sparta_dict = _sparta(tab)
+        sparta_dict = _sparta(row['path'])
         logger.debug('Name %s SPARTA %s', row['name'], sparta_dict)
         qarows.append({'name': row['name'], **sparta_dict})
     if dry_run:
         pprint.pprint(qarows)
     else:
-        upsert_many(mr.db, mr.tables['qa_raw'], qarows, ['name'])
+        upsert_many(mr.db, mr.qa_raw, qarows, ['name'])
 
 
 def qa_psfrec(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
@@ -117,8 +112,7 @@ def qa_psfrec(mr, recipe_name=None, dates=None, skip=True, dry_run=False):
         rows = [row for row in rows if row['name'] not in exists]
     logger.info(f'found {len(rows)} exposures in database to process')
     for row in rows:
-        rawname = row['path']
-        psfrec_dict = _psfrec(rawname)
+        psfrec_dict = _psfrec(row['path'])
         logger.debug('Name %s PSFRec %s', row['name'], psfrec_dict)
         if not dry_run:
             mr.qa_raw.upsert({'name': row['name'], **psfrec_dict}, ['name'])
@@ -132,55 +126,52 @@ def _find_existing_exp(table, key):
 
 
 def _psfrec(filename):
-    import psfrec
-    lbda, fwhm, beta = psfrec.reconstruct_psf(filename)
-    lbref = [500, 700, 900]
-    fwhmref = np.interp(lbref, lbda, fwhm)
-    betaref = np.interp(lbref, lbda, beta)
-    psfdict = {
-        'PR_vers': psfrec.__version__,
-        'PR_fwhmB': fwhmref[0],
-        'PR_fwhmV': fwhmref[1],
-        'PR_fwhmR': fwhmref[2],
-        'PR_betaB': betaref[0],
-        'PR_betaV': betaref[1],
-        'PR_betaR': betaref[2]
+    from psfrec import reconstruct_psf, __version__
+    lbda, fwhm, beta = reconstruct_psf(filename, lmin=500, lmax=900, nl=3)
+    return {
+        'PR_vers': __version__,
+        'PR_fwhmB': fwhm[0], 'PR_fwhmV': fwhm[1], 'PR_fwhmR': fwhm[2],
+        'PR_betaB': beta[0], 'PR_betaV': beta[1], 'PR_betaR': beta[2]
     }
-    return psfdict
 
 
-def _sky(s):
-    skyflux = {}
-    for band, l1, l2 in [['skyB', 4850, 6000],
-                         ['skyV', 6000, 8000],
-                         ['skyR', 8000, 9300]]:
-        flux = float(s['data'][(s['lambda'] >= l1) &
-                               (s['lambda'] <= l2)].mean())
-        skyflux[band] = flux
-    return skyflux
+def _sky(filename):
+    s = Table.read(filename)
+    bands = [['skyB', 4850, 6000],
+             ['skyV', 6000, 8000],
+             ['skyR', 8000, 9300]]
+    return {band: float(np.mean(s['data'][(s['lambda'] >= l1) &
+                                          (s['lambda'] <= l2)]))
+            for band, l1, l2 in bands}
 
 
-def _sparta(tab):
-    res = {}
+def _sparta(rawname):
+    tab = Table.read(rawname, hdu='SPARTA_ATM_DATA')
     klist = [k for k in range(1, 5) if tab[f'LGS{k}_TUR_GND'][0] > 0]
     if len(klist) < 4:
         logger.warning('Mode %d lasers detected', len(klist))
+
+    res = {}
+
     s = [tab[f'LGS{k}_SEEING'].mean() for k in klist]
     res['SP_See'] = float(np.mean(s))
     res['SP_SeeStd'] = float(np.std(s))
     res['SP_SeeMin'] = float(np.min([tab[f'LGS{k}_SEEING'].min() for k in klist]))
-    res['SP_SeeMax'] = float(np.max([tab[f'LGS{k}_SEEING'].max() for k in klist]))         
+    res['SP_SeeMax'] = float(np.max([tab[f'LGS{k}_SEEING'].max() for k in klist]))
+
     g = [tab[f'LGS{k}_TUR_GND'].mean() for k in klist]
     res['SP_Gl'] = float(np.mean(g))
     res['SP_GlStd'] = float(np.std(g))
     res['SP_GLMin'] = float(np.min([tab[f'LGS{k}_TUR_GND'].min() for k in klist]))
-    res['SP_GLMax'] = float(np.max([tab[f'LGS{k}_TUR_GND'].max() for k in klist]))         
+    res['SP_GLMax'] = float(np.max([tab[f'LGS{k}_TUR_GND'].max() for k in klist]))
+
     l = [tab[f'LGS{k}_L0'].mean() for k in klist]
     res['SP_L0'] = float(np.mean(l))
-    res['SP_L0Std'] = float(np.std(l)) 
+    res['SP_L0Std'] = float(np.std(l))
     res['SP_L0Min'] = float(np.min([tab[f'LGS{k}_L0'].min() for k in klist]))
-    res['SP_L0Max'] = float(np.max([tab[f'LGS{k}_L0'].max() for k in klist]))     
+    res['SP_L0Max'] = float(np.max([tab[f'LGS{k}_L0'].max() for k in klist]))
     return res
+
 
 def _imphot(tabname):
     tab = Table.read(tabname)
