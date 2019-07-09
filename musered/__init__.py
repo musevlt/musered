566d166fdd89c0c7f7328a60e82bb5ffc5b6759b
Simon Conseil <contact@saimon.org>
2019-07-09 14:41:38 +0200

    Move logging setup in a function

--- musered/__init__.py
+++ musered/__init__.py
@@ -1,22 +1,19 @@
-# isort:skip_file
 
-import warnings
+def _setup_logging():
+    import warnings
+    from astropy.units import UnitsWarning
+    from mpdaf.log import setup_logging, clear_loggers
 
-from astropy.units import UnitsWarning
-from mpdaf.log import clear_loggers, setup_logging
+    # fmt='[%(process)s] %(levelname)s - %(name)s: %(message)s')
+    setup_logging(name="", level="INFO", color=True, fmt="%(levelname)s %(message)s")
+    clear_loggers("mpdaf")
+    warnings.simplefilter("ignore", category=UnitsWarning)
 
-setup_logging(name="", level="INFO", color=True, fmt="%(levelname)s %(message)s")
-clear_loggers("mpdaf")
 
-from .flags import QAFlags, FLAGS  # noqa
+_setup_logging()
+
+from .flags import FLAGS, QAFlags  # noqa
 from .musered import MuseRed  # noqa
 from .recipes import *  # noqa
 from .settings import RAW_FITS_KEYWORDS  # noqa
 from .version import __version__  # noqa
-
-warnings.simplefilter("ignore", category=UnitsWarning)
-
-# fmt='%(levelname)s - %(name)s: %(message)s')
-# fmt='[%(process)s] %(levelname)s - %(name)s: %(message)s')
-
-del UnitsWarning, setup_logging, clear_loggers
