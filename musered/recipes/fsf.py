e0b38ccd8c44219ca6dd72eeb050612410659d9f
Roland bacon <roland.bacon@univ-lyon1.fr>
2019-07-10 11:15:05 +0200

    update of fsf

--- musered/recipes/fsf.py
+++ musered/recipes/fsf.py
@@ -8,6 +8,7 @@ import sys
 from os.path import join
 
 from mpdaf.MUSE import MoffatModel2
+from mpdaf.log import clear_loggers
 
 import numpy as np
 from astropy.table import Table, Column
@@ -24,29 +25,70 @@ else:
     PSFR_VERSION = muse_psfr.__version__
 
 
-def do_fsf(expname, inputfile, outputfile, imphot_table=None):
+def do_fsf(expname, inputfile, outputfile, imphot_table=None, filters=None):
     logger = logging.getLogger(__name__)
 
     if muse_psfr is None:
         logger.error("muse_psfr is not installed")
         sys.exit(1)
     
-    # Change log  level (NOT WORKING)
-    logpsfrec = logging.getLogger('muse_psfr')
-    logpsfrec.setLevel(logger.level)
+    # clear muse_psfr logger and set to WARNING if not in DEBUG
+    clear_loggers("muse_psfr")
+    if logging.getLogger('').handlers[0].level > logging.DEBUG:
+        logging.getLogger("muse_psfr").setLevel("WARNING")
+    
     
     # run psfrec
     fsfmodel = MoffatModel2.from_psfrec(inputfile)
     
     fwhm = fsfmodel.get_fwhm(np.array(fsfmodel.lbrange))
     beta = fsfmodel.get_beta(np.array(fsfmodel.lbrange))
-    logger.debug('FSF PsfRec model FWHM %.2f-%.2f BETA %.2f-%.2f',fwhm[0],fwhm[1],beta[0],beta[1])
+    logger.info('FSF PsfRec model FWHM %.2f-%.2f BETA %.2f-%.2f',fwhm[0],fwhm[1],beta[0],beta[1])
     
+    kernel = 0
+    if imphot_table is not None:
+        # computing convolution kernel
+        tab = Table.read(imphot_table)
+        imphot_fwhms = tab[[e['filter'] in filters[0] for e in tab]]['fwhm']
+        psfrec_fwhms = fsfmodel.get_fwhm(np.array(filters[1]))
+        kernels = np.sqrt(np.clip(imphot_fwhms**2 - psfrec_fwhms**2,0,np.nan))
+        for band,wave,f1,f2,kernel in zip(filters[0],filters[1],imphot_fwhms,
+                                          psfrec_fwhms,kernels):
+            logger.debug('Filter %s Wave: %.1f IMPHOT FWHM %.2f PSFREC FWHM %.2f Kernel %.2f',
+                         band,wave,f1,f2,kernel)
+        kernel_max = np.mean(kernels)
+        if kernel_max == 0:
+            logger.debug('IMPHOT FSF is smaller than PSFREC FSF, no convolution')
+        else:
+            kernels = [0.6*kernel_max, 0.7*kernel_max, 0.8*kernel_max, 0.9*kernel_max, kernel_max]
+            diffs = []
+            for kernel in kernels:
+                logger.debug('Convolution of FSFmodel with a Gaussian Kernel FWHM: %.2f', kernel)
+                cfsfmodel = fsfmodel.convolve(kernel)
+                final_fwhms = cfsfmodel.get_fwhm(np.array(filters[1]))
+                diff = []
+                for band,wave,f1,f2,f3 in zip(filters[0],filters[1],imphot_fwhms,
+                                                  psfrec_fwhms,final_fwhms):
+                    logger.debug('Filter %s Wave: %.1f IMPHOT FWHM %.2f PSFREC FWHM %.2f COMPUTED %.2f DIFF %.2f',
+                                 band,wave,f1,f2,f3,f3-f1)
+                    diff.append(f3-f1)
+                diffs.append(np.mean(diff))
+            kernel = np.interp(0, diffs, kernels)
+            logger.debug('Final Gaussian Kernel FWHM: %.2f', kernel)
+            cfsfmodel = fsfmodel.convolve(kernel)
+            final_fwhms = cfsfmodel.get_fwhm(np.array(filters[1]))
+            for band,wave,f1,f2,f3 in zip(filters[0],filters[1],imphot_fwhms,
+                                                  psfrec_fwhms,final_fwhms):
+                logger.debug('Filter %s Wave: %.1f IMPHOT FWHM %.2f PSFREC FWHM %.2f FINAL %.2f DIFF %.2f',
+                                     band,wave,f1,f2,f3,f3-f1) 
+            fwhm = cfsfmodel.get_fwhm(np.array(fsfmodel.lbrange))
+            beta = cfsfmodel.get_beta(np.array(fsfmodel.lbrange))
+            logger.info('FSF Final model FWHM %.2f-%.2f BETA %.2f-%.2f',fwhm[0],fwhm[1],beta[0],beta[1])
+            fsfmodel = cfsfmodel      
     
-    
-    tab = Table(names=['NAME','LBDA0','LBDA1','FWHM_B','FWHM_R','BETA_B','BETA_R','NCFWHM','NCBETA'], 
-                dtype=['S25']+6*['f8']+2*['i4'])
-    row = [expname,fsfmodel.lbrange[0],fsfmodel.lbrange[1],fwhm[0],fwhm[1],beta[0],beta[1],
+    tab = Table(names=['NAME','LBDA0','LBDA1','FWHM_B','FWHM_R','BETA_B','BETA_R','KERNEL','NCFWHM','NCBETA'], 
+                dtype=['S25']+7*['f8']+2*['i4'])
+    row = [expname,fsfmodel.lbrange[0],fsfmodel.lbrange[1],fwhm[0],fwhm[1],beta[0],beta[1],kernel,
            len(fsfmodel.fwhm_pol),len(fsfmodel.beta_pol)]
     for k,val in enumerate(fsfmodel.fwhm_pol):
         tab.add_column(Column(name=f"FWHM_P{k}", dtype='f8'))
@@ -71,14 +113,15 @@ class FSF(PythonRecipe):
 
     default_params = dict()
 
-    def _run(self, flist, *args, imphot_tables=None, **kwargs):
+    def _run(self, flist, *args, imphot_tables=None, filters=None, **kwargs):
         expname = get_exp_name(flist[0])
         out = join(self.output_dir, f"FSF.fits")
         if imphot_tables is not None:
             self.imphot_table = imphot_tables.get(expname)
         else:
             self.imphot_table = None
-        do_fsf(expname, flist[0], out, imphot_table=self.imphot_table, **self.param)
+        do_fsf(expname, flist[0], out, imphot_table=self.imphot_table, 
+               filters=filters, **self.param)
         return out
 
     def dump(self, include_files=False, json_col=False):
